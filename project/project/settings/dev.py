810a022150381812dd37728eb0a531586eccab86
Justice <justice.douglas@hey.com>
2021-03-08 08:53:45 -0500

    Created Lovelace Library project via Codered CMS install.

---
+++ project/project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'llq_svfjefhprh2(u(nu8j==ih%$o)os_cn^%w^#&e=2mm#)_b'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
